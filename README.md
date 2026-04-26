95120cd43fdf191b6cb3fa170bf4a9bb49703053
gmsowers <gmsowers@gmail.com>
2026-04-26 18:33:22 -0400

    Add docs folder.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mbopt library
+
+This is a C++ library for formulating mass balance models of chemical processes and solving them with [Ipopt](https://github.com/coin-or/Ipopt). It is designed to be called from a Lua script. It also provides a binary that can be invoked with a Lua script as an argument.
+
+This package is unregistered, which means it's not in the Julia package registry. If you happen to discover it and want to use it, do this:
+
+[Mbopt documentation](https://gmsowers.github.io/mbopt/).
---
+++ docs/unitset.lua
@@ -0,0 +1,46 @@
+---@diagnostic disable: undefined-global
+
+kinds = {
+    massflow = { "kg/hr",    "kg/hr"    },  -- {base_unit_str, default_unit_str}
+    massfrac = { "massfrac", "massfrac" },
+    moleflow = { "kmol/hr",  "kmol/hr"  },
+    molewt   = { "kg/kmol",  "kg/kmol"  },
+    frac     = { "frac",     "frac"     },
+    massval  = { "$/kg",     "$/kg"     },
+    flowval  = { "$/hr",     "$/hr"     },
+    count    = { "#",        "#"        }
+}
+units = {
+    massflow = {
+        { "kg/hr", 1.0,          0.0 },     -- {unit_str, unit_ratio, unit_offset} unit_offset optional
+        { "lb/hr", 1.0 / 2.20462     },
+        { "t/hr" , 1000.0            }
+    },
+    massfrac = {
+        { "massfrac", 1.0         },
+        { "mass%",    1.0 / 100.0 }
+    },
+    moleflow = {
+        { "kmol/hr", 1.0,        0.0 }
+    },
+    molewt = {
+        { "kg/kmol", 1.0,        0.0 }
+    },
+    frac = {
+        { "frac", 1.0         },
+        { "%",    1.0 / 100.0 }
+    },
+    massval = {
+        { "$/kg",   1.0     },
+        { "$/lb",   2.20462 },
+        { "cts/lb", 2.20462 / 100.0}
+    },
+    flowval = {
+        { "$/hr",  1.0        },
+        { "$/day", 1.0 / 24.0 }
+    },
+    count = {
+        { "#", 1.0}
+    }
+}
+return UnitSet(kinds, units)
